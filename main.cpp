370e760c8291616f3c360d01506eb8793859d100
Anderson <wildami_1994@hotmail.com>
2017-03-15 17:13:20 -0430

    modifique el main, ahora el programa permite agregar los datos de la vaca

--- main.cpp
+++ main.cpp
@@ -2,16 +2,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <conio.h>
-
+#include "cows.h"
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-
 int main(int argc, char *argv[]) {
 	
-
-	int opc=0;
-
+	cows vacas[10];
+	int opc=0,cont=0;
+	string nombre;
+	float peso,lts_leche;
+	int raza=1;
+		
 	do{	
 		cout<<"\tElija una Opcion"<<endl<<endl;
 		cout<<"1- Agregar Vaca"<<endl;
@@ -25,7 +27,32 @@ int main(int argc, char *argv[]) {
 		
 			case 1:
 				cout<<"\tIngrese los Datos de la Vaca"<<endl<<endl;
-				
+				int v,a,i;
+				cout<<"Nombre: ";
+				cin>>nombre;
+				vacas[cont].setNombre(nombre);
+			
+				cout<<"\nRaza: "<<endl<<endl;
+				cout<<"   1- Simmental"<<endl;
+				cout<<"   2- Pasiega"<<endl;
+				cout<<"   3- Jersey"<<endl;
+				cout<<"   4- Guernsey"<<endl;
+				cin>>raza;
+				vacas[cont].setRaza(raza);
+			
+				cout<<"\nPeso (Kg): ";
+				cin>>peso;
+				vacas[cont].setPeso(peso);
+			
+				cout<<"\nLitros de leche: ";
+				cin>>lts_leche;
+				vacas[cont].setLitroLeche(lts_leche);
+			
+				cont++;
+				system("cls");
+				cout<<"\n\n\tREGISTRO EXITOSO"<<endl<<endl;
+				system("PAUSE");
+				system("cls");
 			break;
 			
 			case 2:
